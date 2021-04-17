e8f60ac028832ebf4839b752d4ad284123a0b340
GameDev-Mark <gamedev.markd@gmail.com>
2021-04-17 21:47:06 +0200

    Rotating Catapult

--- CatapultPrototype/Assets/Scripts/CatapultRotator.cs
+++ CatapultPrototype/Assets/Scripts/CatapultRotator.cs
@@ -1,37 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CatapultRotator : MonoBehaviour
 {
-    public GameObject endCatapult;
+    int catapultForwardRotSpeed;
+    int catapultReverseRotSpeed;
+    int speedIncrease;
 
+    bool slowSpeed;
 
-    // Start is called before the first frame update
+    // unitys start function
     void Start()
     {
-        
+        catapultForwardRotSpeed = 40;
+        catapultReverseRotSpeed = 30;
+        speedIncrease = 3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(1))
-        {
-            Vector3 xRotAngle = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
-            //xRotAngle.z = (xRotAngle.z > 180) ? xRotAngle.z - 360 : xRotAngle.z;
+        CatapultRotating();
+    }
 
-            transform.Rotate(xRotAngle * 4 * Time.deltaTime);
-            Debug.Log("DOWN");
+    // rotating the catapult arm , shooting and resetting
+    public void CatapultRotating()
+    {
+        Vector3 zRotAngle = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z); // ref to rotations
+        Vector3 zTopRotAngle = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, -90f); // when rotator reaches top 
+        Vector3 zBotRotAngle = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 20f); // when rotator reaches bottom 
+        zRotAngle.z = (zRotAngle.z > 180) ? zRotAngle.z - 360 : zRotAngle.z; // lets the angle be rotated accordinaly 
+
+        Debug.Log(catapultForwardRotSpeed);
+        if (Input.GetMouseButton(1)) // CATAPULT GO DOWN
+        {
+            transform.RotateAround(transform.position, Vector3.forward, catapultReverseRotSpeed * Time.deltaTime);
+        }
+        else if(zRotAngle.z <= -90f)
+        {
+            transform.localEulerAngles = zTopRotAngle;
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0)) // CATAPULT GO UP
         {
-            Vector3 xRotAngle = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
-            xRotAngle.z = (xRotAngle.z > 180) ? xRotAngle.z - 360 : xRotAngle.z;
+            canClickMouse = true;
+            catapultForwardRotSpeed += speedIncrease;
+            transform.RotateAround(transform.position, Vector3.back, catapultForwardRotSpeed * Time.deltaTime);
+        }
+        else if (zRotAngle.z >= 20f)
+        {
+            transform.localEulerAngles = zBotRotAngle;
+            canClickMouse = false;
 
-            transform.Rotate(xRotAngle *- 4 * Time.deltaTime);
-            Debug.Log("UP");
+        }
+        else if (canClickMouse == false)
+        {
+            catapultForwardRotSpeed = 40;
         }
     }
 }
