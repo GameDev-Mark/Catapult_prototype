3516d6109553f602fba32093389e4abff583081a
GameDev-Mark <gamedev.markd@gmail.com>
2021-05-09 18:36:43 +0200

    update

--- CatapultPrototype/Assets/Scripts/GameCon.cs
+++ CatapultPrototype/Assets/Scripts/GameCon.cs
@@ -1,14 +1,44 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System;
+using TMPro;
 
 public class GameCon : MonoBehaviour
 {
+    GameObject objectThrown;
+    float objectsInPlay;
+    public TMP_Text objectInPlayText;
+
+    void Start()
+    {
+        objectsInPlay = 0f;
+
+    }
+
     // unitys update function
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        objectThrown = GameObject.FindWithTag("ObjectThrown");
+
+        if (Input.GetKeyDown(KeyCode.E))
         {
             SceneManager.LoadScene(0);
         }
+
+        // check if object being thrown is active in hieracrhy // check if object is not in active in hierarchy, catch the null___________
+        try
+        {
+            if(objectThrown.gameObject.activeInHierarchy)
+                objectsInPlay++;
+            Debug.Log("true");
+        }
+        catch(NullReferenceException) 
+        {
+            if (!objectThrown.gameObject.activeInHierarchy)
+                objectsInPlay--;
+            Debug.Log("null");
+        }
+
+
     }
 }
